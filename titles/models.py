ea4e08bf5aba2fc10f32eae822f989a3b2dafe36
Vineet Dorikar <vineetdorikar06@gmail.com>
2025-01-30 00:46:54 +0530

    simplified backend for understanding how data flows

--- titles/models.py
+++ titles/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Title(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, blank=False, null=False)
 
     def __str__(self):
         return self.title
--- titles/views.py
+++ titles/views.py
@@ -5,13 +5,16 @@ from .utils import preprocess_title, compute_similarity
 def check_similarity(request):
     if request.method == 'POST':
         new_title = request.POST.get('title')
+
         existing_titles = [t.title for t in Title.objects.all()]
-        
-        preprocessed_title = preprocess_title(new_title)
-        cosine_sim, fuzzy_sim = compute_similarity(new_title, existing_titles)
-        
-        results = [{"title": t, "cosine_similarity": float(cosine), "fuzzy_similarity": fuzzy} 
-                   for t, cosine, fuzzy in zip(existing_titles, cosine_sim, fuzzy_sim)]
-        
+        var = ""
+        if new_title in existing_titles:
+            var = "high"
+            results = {"title": new_title, "msg": "yes exists","var": var,"existing_titles": existing_titles}
+        else: 
+            var = "less"
+            results = {"title": new_title, "msg": "no exists","var": var, "existing_titles": existing_titles}
+
+    
         return render(request, 'titles/similarity_results.html', {'results': results})
     return render(request, 'titles/similarity_form.html')
